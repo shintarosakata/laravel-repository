f5673c1b73844001d5c8cf0ed59c80556f3f47f6
shintaro.sakata <shintaro.sakata@leverages.jp>
2019-09-19 09:31:02 +0900

    init

---
+++ src/LaravelRepositoryServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace shintarosakata\LaravelRepository;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelRepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
