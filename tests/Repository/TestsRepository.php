157d0e6ac7983bea69e86b649f9a6af1f09e7b6a
shintarosakata <shintaro.sakata@leverages.jp>
2019-09-22 14:50:02 +0900

    add repository Test

---
+++ tests/Repository/TestsRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace shintarosakata\LaravelRepository\Tests\Repository;
+
+use shintarosakata\LaravelRepository\Repository\Repository;
+
+class TestsRepository extends Repository
+{
+    protected $table = 'tests';
+}
